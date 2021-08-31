4da517f3ea8bf10ea69060e3bc16bec7622d742c
pablopison <pablo.pison@gmail.com>
2021-08-31 15:37:56 -0300

    Renombrar fotos

--- ProyectoBarrios/BackEnd/servidor/index.js
+++ ProyectoBarrios/BackEnd/servidor/index.js
@@ -112,91 +112,91 @@ const barrios = [
     {
         nombre: "POCITOS",
         fotos: [
-            'DSC_0003', 'DSC_0005', 'DSC_0020', 'DSC_0024', 
-            'DSC_9999', 'DSC_0032', 'DSC_9998', 'DSC_9983',
-            'DSC_9985', 'DSC_0011', 'DSC_0049', 'DSC_0051']
+            'Pocitos_01', 'Pocitos_02', 'Pocitos_03', 'Pocitos_04', 
+            'Pocitos_05', 'Pocitos_06', 'Pocitos_07', 'Pocitos_08',
+            'Pocitos_09', 'Pocitos_10', 'Pocitos_11', 'Pocitos_12']
     },
     {
         nombre: "PRADO",
         fotos: [
-            'DSC_0931', 'DSC_1679', 'DSC_4888', 'DSC_4988', 
-            'DSC_1227', 'DSC_1228', 'DSC_4958', 'DSC_4977',
-            'DSC_1662', 'DSC_1689', 'DSC_1696', 'DSC_1723']
+            'Prado_01', 'Prado_02', 'Prado_03', 'Prado_04', 
+            'Prado_05', 'Prado_06', 'Prado_07', 'Prado_08',
+            'Prado_09', 'Prado_10', 'Prado_11', 'Prado_12']
     },
     {
         nombre: "PARQUE RODO",
         fotos: [
-            'DSC_7904', 'DSC_7913', 'DSC_7923', 'DSC_7925', 
-            'DSC_7927', 'DSC_7938', 'DSC_7950', 'DSC_7959',
-            'DSC_7965', 'DSC_7967', 'DSC_7994', 'DSC_8043',
-            'DSC_7987', 'DSC_7992', 'DSC_8021', 'DSC_8026']
+            'Parque_Rodo_01', 'Parque_Rodo_02', 'Parque_Rodo_03', 'Parque_Rodo_04', 
+            'Parque_Rodo_05', 'Parque_Rodo_06', 'Parque_Rodo_07', 'Parque_Rodo_08',
+            'Parque_Rodo_09', 'Parque_Rodo_10', 'Parque_Rodo_11', 'Parque_Rodo_12',
+            'Parque_Rodo_13', 'Parque_Rodo_14', 'Parque_Rodo_15', 'Parque_Rodo_16']
     },
     {
         nombre: "AGUADA",
         fotos: [
-            'Aguada1', 'Aguada2', 'DSC_8402', 'DSC_8492', 
-            'DSC_1148', 'DSC_1158', 'DSC_2505', 'DSC_2327',
-            'DSC_2324', 'DSC_2503', 'DSC_2239', 'DSC_8369']
+            'Aguada_01', 'Aguada_02', 'Aguada_03', 'Aguada_04', 
+            'Aguada_05', 'Aguada_06', 'Aguada_07', 'Aguada_08',
+            'Aguada_09', 'Aguada_10', 'Aguada_11', 'Aguada_12']
     },
     {
         nombre: "CENTRO",
         fotos: [
-            'DSC_1215', 'DSC_1300', 'DSC_1197', 'DSC_8383', 
-            'DSC_8367', 'DSC_2607', 'DSC_1880', 'DSC_2618',
-            'DSC_1874', 'DSC_1850', 'DSC_1178', 'DSC_1853',
-            'DSC_8651', 'DSC_1854', 'DSC_8593', 'DSC_8647']
+            'Centro_01', 'Centro_02', 'Centro_03', 'Centro_04', 
+            'Centro_05', 'Centro_06', 'Centro_07', 'Centro_08',
+            'Centro_09', 'Centro_10', 'Centro_11', 'Centro_12',
+            'Centro_13', 'Centro_14', 'Centro_15', 'Centro_16']
     },
     {
         nombre: "CAPURRO",
         fotos: [
-            'DSC_0723', 'DSC_1590', 'DSC_1532', 'DSC_0813', 
-            'DSC_1417', 'DSC_1168', 'DSC_0768', 'DSC_1475',
-            'DSC_1570', 'DSC_1539', 'DSC_1166', 'DSC_1486',
-            'DSC_0796', 'DSC_0804', 'DSC_0820', 'DSC_0716']
+            'Capurro_01', 'Capurro_02', 'Capurro_03', 'Capurro_04', 
+            'Capurro_05', 'Capurro_06', 'Capurro_07', 'Capurro_08',
+            'Capurro_09', 'Capurro_10', 'Capurro_11', 'Capurro_12',
+            'Capurro_13', 'Capurro_14', 'Capurro_15', 'Capurro_16']
     },
     {
         nombre: "BARRIO SUR",
         fotos: [
-            'DSC_0295', 'DSC_0285', 'DSC_0334', 'DSC_0288', 
-            'DSC_0283', 'DSC_0347', 'DSC_0263', 'DSC_0355',
-            'DSC_0159', 'DSC_1064', 'DSC_1205', 'DSC_1421',
-            'DSC_2093', 'DSC_2107', 'DSC_2117', 'DSC_1509',
-            'DSC_0308', 'DSC_1419', 'DSC_1246', 'DSC_0352']
+            'Barrio_Sur_01', 'Barrio_Sur_02', 'Barrio_Sur_03', 'Barrio_Sur_04', 
+            'Barrio_Sur_05', 'Barrio_Sur_06', 'Barrio_Sur_07', 'Barrio_Sur_08',
+            'Barrio_Sur_09', 'Barrio_Sur_10', 'Barrio_Sur_11', 'Barrio_Sur_12',
+            'Barrio_Sur_13', 'Barrio_Sur_14', 'Barrio_Sur_15', 'Barrio_Sur_16',
+            'Barrio_Sur_17', 'Barrio_Sur_18', 'Barrio_Sur_19', 'Barrio_Sur_20']
     },
     {
         nombre: "PARQUE BATLLE",
         fotos: [
-            'DSC_1143', 'DSC_1159', 'DSC_9033', 'DSC_1154', 
-            'DSC_1173', 'DSC_8872', 'DSC_1163', 'DSC_8906',
-            'DSC_1165', 'DSC_9015', 'DSC_1172', 'DSC_8943',
-            'DSC_1182', 'DSC_1178', 'DSC_8885', 'DSC_8902']
+            'Parque_Batlle_01', 'Parque_Batlle_02', 'Parque_Batlle_03', 'Parque_Batlle_04', 
+            'Parque_Batlle_05', 'Parque_Batlle_06', 'Parque_Batlle_07', 'Parque_Batlle_08',
+            'Parque_Batlle_09', 'Parque_Batlle_10', 'Parque_Batlle_11', 'Parque_Batlle_12',
+            'Parque_Batlle_13', 'Parque_Batlle_14', 'Parque_Batlle_15', 'Parque_Batlle_16']
     },
     {
         nombre: "CORDON",
         fotos: [
-            'Cordon1', 'Cordon2', 'Cordon3', 'Cordon4', 
-            'Cordon5', 'Cordon6', 'Cordon7', 'Cordon8',
-            'Cordon9', 'Cordon10', 'Cordon11', 'Cordon12']
+            'Cordon_01', 'Cordon_02', 'Cordon_03', 'Cordon_04', 
+            'Cordon_05', 'Cordon_06', 'Cordon_07', 'Cordon_08',
+            'Cordon_09', 'Cordon_10', 'Cordon_11', 'Cordon_12']
     },
     {
         nombre: "TRES CRUCES",
         fotos: [
-            'TresCruces1', 'TresCruces2', 'TresCruces3', 'TresCruces4', 
-            'TresCruces5', 'TresCruces6', 'TresCruces7', 'TresCruces8',
-            'TresCruces9', 'TresCruces10', 'TresCruces11', 'TresCruces12']
+            'Tres_Cruces_01', 'Tres_Cruces_02', 'Tres_Cruces_03', 'Tres_Cruces_04', 
+            'Tres_Cruces_05', 'Tres_Cruces_06', 'Tres_Cruces_07', 'Tres_Cruces_08',
+            'Tres_Cruces_09', 'Tres_Cruces_10', 'Tres_Cruces_11', 'Tres_Cruces_12']
     },
     {
         nombre: "MERCADO MODELO",
         fotos: [
-            'MercadoModelo1', 'MercadoModelo2', 'MercadoModelo3', 'MercadoModelo4', 
-            'MercadoModelo5', 'MercadoModelo6', 'MercadoModelo7', 'MercadoModelo8']
+            'Mercado_Modelo_01', 'Mercado_Modelo_02', 'Mercado_Modelo_03', 'Mercado_Modelo_04', 
+            'Mercado_Modelo_05', 'Mercado_Modelo_06', 'Mercado_Modelo_07', 'Mercado_Modelo_08']
     },
     {
         nombre: "BELLA VISTA",
         fotos: [
-            'DSC_2307', 'DSC_3569', 'DSC_2309', 'DSC_3555', 
-            'DSC_0732', 'DSC_0734', 'DSC_0764', 'DSC_8445',
-            'DSC_0776', 'DSC_0789', 'DSC_0738', 'DSC_0766']
+            'Bella_Vista_01', 'Bella_Vista_02', 'Bella_Vista_03', 'Bella_Vista_04', 
+            'Bella_Vista_05', 'Bella_Vista_06', 'Bella_Vista_07', 'Bella_Vista_08',
+            'Bella_Vista_09', 'Bella_Vista_10', 'Bella_Vista_11', 'Bella_Vista_12']
     },
 ] 
 
